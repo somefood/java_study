ee6c6cbef905064d749112104fb61e03c01b1ea4
Seokju Hong <iamseokjuhong@gmail.com>
2020-11-16 15:06:39 +0900

    데이터와 연산

---
+++ src/Datatype.java
@@ -0,0 +1,20 @@
+public class Datatype {
+    public static void main(String[] args){
+        System.out.println(6);
+        System.out.println("six");
+
+        System.out.println("6");
+
+        System.out.println(6+6);
+        System.out.println("6"+"6");
+
+        System.out.println(6*6);
+//        System.out.println("6"*"6");
+
+        System.out.println("1111".length());
+
+        System.out.println("Hello World!");
+        System.out.println('H');
+        System.out.println("H");
+    }
+}
