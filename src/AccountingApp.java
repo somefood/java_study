af247f52ab6a725dc1851560edb5624c8755068b
Seokju Hong <iamseokjuhong@gmail.com>
2020-11-17 13:37:41 +0900

    나의 앱 만들기 2

--- src/AccountingApp.java
+++ src/AccountingApp.java
@@ -1,12 +1,36 @@
 public class AccountingApp {
     public static void main(String[] args) {
-        System.out.println("Value of supply : "+ 12345.0);
-        System.out.println("VAT : "+ (12345.0 * 0.1));
-        System.out.println("Total : "+ (12345.0 + 12345.0 * 0.1));
-        System.out.println("Expense : "+ (12345.0 * 0.3));
-        System.out.println("Income : "+ (12345.0 - 12345.0 * 0.3));
-        System.out.println("Dividend1 : "+ (12345.0 - 12345.0 * 0.3) * 0.5);
-        System.out.println("Dividend2 : "+ (12345.0 - 12345.0 * 0.3) * 0.3);
-        System.out.println("Dividend3 : "+ (12345.0 - 12345.0 * 0.3) * 0.2);
+        double valueOfSupply = Double.parseDouble(args[0]);
+        double vatRate = 0.1;
+        double expenseRate = 0.3;
+        double vat = valueOfSupply * vatRate;
+        double total = valueOfSupply + vat;
+        double expense = valueOfSupply * expenseRate;
+        double income = valueOfSupply - expense;
+
+        double [] dividendRates = new double[3];
+        dividendRates[0] = 0.5;
+        dividendRates[1] = 0.3;
+        dividendRates[2] = 0.2;
+
+        double dividend1 = income * dividendRates[0];
+        double dividend2 = income * dividendRates[1];
+        double dividend3 = income * dividendRates[2];
+
+        int i = 0;
+        while ( i < dividendRates.length){
+            System.out.println("Dividend : " + dividendRates[i]);
+            i = i + 1;
+        }
+
+        System.out.println("Value of supply :" + valueOfSupply);
+        System.out.println("VAT :" + vat);
+        System.out.println("Total :" + total);
+        System.out.println("Expense : " + expense);
+        System.out.println("Income : " + income);
+        System.out.println("Dividend 1 : " + dividend1);
+        System.out.println("Dividend 2 : " + dividend2);
+        System.out.println("Dividend 3 : " + dividend3);
+
     }
 }
