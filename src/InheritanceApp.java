313cbde16bd44f166d664f9ade6b6f49df0070cb
Seokju Hong <iamseokjuhong@gmail.com>
2020-11-18 20:24:56 +0900

    this, super

--- src/InheritanceApp.java
+++ src/InheritanceApp.java
@@ -3,9 +3,9 @@ class Cal {
         return v1 + v2;
     }
 
-    // Overloading, 과적하기 - 같은 메소드 명으로 다른 인자 개수를 받을 수 있다.
+    // Overloading, 과적하기 - 같은 메소드 명으로 다른 인자 개를 받을 수 있다.
     public int sum(int v1, int v2, int v3){
-        return v1 + v2 + v3;
+        return this.sum(v1, v2) + v3;  // 자신의 sum 메소드를 찾아서 사용
     }
 }
 
@@ -13,11 +13,7 @@ class Cal2 extends Cal {
     // Overriding 부모꺼 덮어 씌우기
     public int sum(int v1, int v2){
         System.out.println("Cal2!!");
-        return v1+ v2;
-    }
-
-    public int sum(int v1, int v2, int v3){
-        return v1 + v2 + v3;
+        return super.sum(v1, v2); // 부모클래스 메소드 호출
     }
 
     public int minus(int v1, int v2){
