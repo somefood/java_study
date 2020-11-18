dc515896189821b1d69c07bee3a5a234e67fe9a2
Seokju Hong <iamseokjuhong@gmail.com>
2020-11-18 18:41:48 +0900

    인스턴스

--- src/MyOOP.java
+++ src/MyOOP.java
@@ -1,12 +1,12 @@
-class Print {
-    public static String delimiter = "";
-    public static void A(){
+class Printt {
+    public String delimiter = "";
+    public void A(){
         System.out.println(delimiter);
         System.out.println("A");
         System.out.println("A");
     }
 
-    public static void B(){
+    public void B(){
         System.out.println(delimiter);
         System.out.println("B");
         System.out.println("B");
@@ -15,16 +15,19 @@ class Print {
 
 public class MyOOP {
     public static void main(String[] args) {
-        Print.delimiter = "---";
-        Print.A();
-        Print.A();
-        Print.A();
-        Print.A();
 
-        Print.delimiter = "***";
-        Print.B();
-        Print.B();
-        Print.B();
-        Print.B();
+        Printt p1 = new Printt();
+        p1.delimiter = "*****"; // static 제거해야 인스턴스께 됨
+        p1.A();
+        p1.A();
+        p1.B();
+        p1.B();
+
+        Printt p2 = new Printt();
+        p2.delimiter = "-----"; // static 제거해야 인스턴스께 됨
+        p2.A();
+        p2.A();
+        p2.B();
+        p2.B();
     }
 }
